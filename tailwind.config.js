a8ce9885362f4c0118534ec8588f1a2d03d2a3b0
Andrea Brezanova <55324009+Andrea-Brezanova@users.noreply.github.com>
2025-01-03 16:34:42 +0100

    Added fonts and custom font colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@
 module.exports = {
   content: [
     './index.html',
-    './public/**/*.{html,js}',
+    './public/*.{html,js}',
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bookmark-purple": "#5267DF",
+        "bookmark-red": "#FA5959",
+        "bookmark-blue": "#242A45",
+        "bookmark-grey": "#9194A2",
+        "bookmark-white": "#f7f7f7",
+      }
+    },
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+    },
     container: {
       center: true,
       padding: '1rem',
